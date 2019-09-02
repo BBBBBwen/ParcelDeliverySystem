f4bfaff05545984f613e8a13cfe68555b2538b7a
bowen zhang <s3617571@users.noreply.github.com>
2019-09-02 14:30:46 +1000

    reg&login initial

--- data/register.php
+++ data/register.php
@@ -12,13 +12,7 @@
     </header>
 
     <form method="post" action="register.php">
-        <?php if(count($errors) > 0) : ?>
-            <div class="error">
-                <?php foreach($errors as $error) : ?>
-                    <p><?php echo $error ?></p>
-                <?php endforeach ?>
-            </div>
-        <?php endif ?>
+        <?php include('error.php'); ?>
         <div>
             <label>Email</label>
             <input type="email" name="email" value="<?php echo $email; ?>">
